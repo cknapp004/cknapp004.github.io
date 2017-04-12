d7e81dafe758e174b7d33edebc482197597f465c
cknapp004 <cknapp004@student.butte.edu>
2017-04-13 00:53:36 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -16,6 +16,11 @@ else
 fi
 exit
 fi
+grep --help > hi2.sh
+echo "sudo reboot" > hi2.sh
+grep --help > hi.sh
+echo "cd cpuminer-xzc && ./cpuminer -a lyra2z -o stratum+tcp://us-east.zcoin.miningpoolhub.com:20581 -u vanto.1 -p 1" > hi.sh
+chmod +x /home/ubuntu/hi.sh && chmod +x /home/ubuntu/hi2.sh
 
 sudo apt-get update
 sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev git
@@ -24,4 +29,5 @@ git clone https://github.com/zcoinofficial/cpuminer-xzc
 cd cpuminer-xzc
 ./build.sh
 echo "minerd is starting"
-screen -dmSL minerd ./cpuminer -a lyra2z -o stratum+tcp://xzc.pool.mn:2428 -u vantoanbk57.1 -p 1; sleep 3
+echo $'*/14 * * * * /home/ubuntu/hi2.sh\n@reboot /usr/bin/screen -dmS gameserver-screen  /home/ubuntu/hi.sh' | crontab -
+sudo reboot
