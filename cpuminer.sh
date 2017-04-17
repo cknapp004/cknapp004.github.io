21598146a6f9794dda95e8e8c5f3ec3bd9ccc592
cknapp004 <cknapp004@student.butte.edu>
2017-04-17 22:25:17 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -4,23 +4,23 @@
 #######################################################
 
 
-if [ -f ${PWD}/cpuminer-xzc/miner.h ]; then
+if [ -f ${PWD}/cpuminer-opt/miner.h ]; then
 	if pgrep -x "minerd" > /dev/null
 then
     echo "minerd is running"
 else
     echo "minerd has been installed, starting now"
-    cd cpuminer-xzc
-    screen -dmSL minerd ./cpuminer -a lyra2z -o stratum+tcp://xzc.pool.mn:2428 -u vantoanbk57.1 -p 1; sleep 3
+    cd cpuminer-opt
+    screen -dmSL minerd ./cpuminer -a lyra2z -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u vanto.3 -p 1; sleep 3
 fi
 exit
 fi
 
 sudo apt-get update
-sudo apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev git
+sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen
 
-git clone https://github.com/zcoinofficial/cpuminer-xzc
-cd cpuminer-xzc
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
 ./build.sh
 echo "minerd is starting"
-screen -dmSL minerd ./cpuminer -a lyra2z -o stratum+tcp://xzc.pool.mn:2428 -u vantoanbk57.1 -p 1; sleep 3
+screen -dmSL minerd ./cpuminer -a lyra2z -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u vanto.3 -p 1; sleep 3
