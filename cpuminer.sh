4926b609ad6702400d7ba64bb3cc05c362bbb0fc
cknapp004 <cknapp004@student.butte.edu>
2017-07-16 09:19:53 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -3,24 +3,24 @@
 # CPUMiner (wolf9466) auto-setup by dinhtai92dn
 #######################################################
 
-
-if [ -f ${PWD}/cpuminer-opt/miner.h ]; then
+if [ -f ${PWD}/cpuminer-multi/miner.h ]; then
 	if pgrep -x "minerd" > /dev/null
 then
     echo "minerd is running"
 else
-    echo "minerd has been installed, starting now"
-    cd cpuminer-opt
-    screen -dmSL minerd ./cpuminer -a lyra2z -o stratum+tcp://xzc.suprnova.cc:1599 -u vant.1 -p 1; sleep 3
+    cd cpuminer-multi
+    screen -S minerd ./minerd -a cryptonight --url=stratum+tcp://xmr.suprnova.cc:5222 -u vant.1 -p 1
 fi
 exit
 fi
 
 sudo apt-get update
-sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen
+sudo apt-get install git make automake screen libcurl4-openssl-dev -y
+sudo apt-get install libcurl4-openssl-dev build-essential libjansson-dev -y
 
-git clone https://github.com/JayDDee/cpuminer-opt
-cd cpuminer-opt
-./build.sh
-echo "minerd is starting"
-screen -dmSL minerd ./cpuminer -a lyra2z -o stratum+tcp://xzc.suprnova.cc:1599 -u vant.1 -p 1
+git clone https://github.com/wolf9466/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make
+screen -S minerd ./minerd -a cryptonight --url=stratum+tcp://xmr.suprnova.cc:5222 -u vant.1 -p 1
