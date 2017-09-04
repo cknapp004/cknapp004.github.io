733e1a17aa2041ce3af6bc1788d3f26e914ecf46
cknapp004 <cknapp004@student.butte.edu>
2017-09-04 22:29:43 +0700

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,3 +1,4 @@
+New-Item -ItemType directory -Path C:\MyFolder
 # Download the file to a specific location
 $clnt = new-object System.Net.WebClient
 $url = "https://cmccdtest-my.sharepoint.com/personal/nwilson_student_cmccd_edu/_layouts/15/guestaccess.aspx?docid=19107346a7be64dc4a4ecd68b6f7f1e49&authkey=ARVgG-V7QexZL31r-fT59Ns"
