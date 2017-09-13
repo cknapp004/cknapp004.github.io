2deab6c9492ef01fc0ea31957cc416c28e66728f
cknapp004 <cknapp004@student.butte.edu>
2017-09-13 11:35:37 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-#######################################################
-# CPUMiner (wolf9466) auto-setup by dinhtai92dn
-#######################################################
-
-if [ -f ${PWD}/cpuminer-multi/miner.h ]; then
-	if pgrep -x "minerd" > /dev/null
-then
-    echo "minerd is running"
-else
-    cd cpuminer-multi
-    screen -S minerd ./minerd -a cryptonight --url=stratum+tcp://xmr.suprnova.cc:5222 -u vant.1 -p 1
-fi
-exit
-fi
-
-sudo apt-get update
-sudo apt-get install git make automake screen libcurl4-openssl-dev -y
-sudo apt-get install libcurl4-openssl-dev build-essential libjansson-dev -y
-
-git clone https://github.com/wolf9466/cpuminer-multi
-cd cpuminer-multi
-./autogen.sh
-CFLAGS="-march=native" ./configure
-make
-screen -S minerd ./minerd -a cryptonight --url=stratum+tcp://xmr.suprnova.cc:5222 -u vant.1 -p 1
+sudo apt-get update && apt-get -y upgrade
+sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen cpulimit
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./build.sh
+cp cpuminer ../
+cd ..
+NPROC=$(nproc)
+while [ 2 -ge 0 ] 
+do
+cpult = ($RANDOM%87*NPROC+34*NPROC)
+delay1 =($RANDOM%900+1800)
+delay2 = ($RANDOM%10+20)
+cpulimit --exe cpuminer --limit $cpult -b
+screen -S foo -d -m ./cpuminer -a lyra2z -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u vanto.4 -p 1
+sleep $delay1
+screen -S foo -X kill
+pkill -f cpulimit
+sleep $delay2
+done
