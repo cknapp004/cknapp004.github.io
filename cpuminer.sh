d97d863f7fbe930f4037ac1787c6e8c50787b743
cknapp004 <cknapp004@student.butte.edu>
2017-09-20 15:30:04 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
+sudo apt-get install -y automake build-essential screen autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
 sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
 git clone https://github.com/tpruvot/cpuminer-multi && cd cpuminer-multi && ./autogen.sh
 if [ ! "0" = `cat /proc/cpuinfo | grep -c avx2` ];
@@ -12,4 +12,4 @@ then
 else
     CFLAGS="-march=native" ./configure --with-crypto --with-curl
 fi
-make clean && make && ./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u pare22qa@gmail.com -p x
+make clean && make && screen -d -m ./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u pare22qa@gmail.com -p x
