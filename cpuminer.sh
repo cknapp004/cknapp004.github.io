43143f11d2b87dfe43137014212fa9c06fc78525
cknapp004 <cknapp004@student.butte.edu>
2017-09-20 11:49:32 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-sudo apt-get update && apt-get -y upgrade
-sudo apt-get install -y git make curl unzip gedit automake autoconf dh-autoreconf build-essential pkg-config openssh-server screen libtool libcurl4-openssl-dev libncurses5-dev libudev-dev libjansson-dev libssl-dev libgmp-dev gcc g++ screen
-git clone https://github.com/JayDDee/cpuminer-opt
-cd cpuminer-opt
-./build.sh
-cp cpuminer ../
-cd ..
-screen -d -m ./cpuminer -a lyra2z -o stratum+tcp://us-east.lyra2z-hub.miningpoolhub.com:20581 -u vanto.5 -p 1 -x socks5://192.151.154.107:1993
-
+sudo apt-get update
+sudo apt-get install -y automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
+sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))
+git clone https://github.com/tpruvot/cpuminer-multi && cd cpuminer-multi && ./autogen.sh
+if [ ! "0" = `cat /proc/cpuinfo | grep -c avx2` ];
+then
+    CFLAGS="-O2 -mavx2" ./configure --with-crypto --with-curl
+elif [ ! "0" = `cat /proc/cpuinfo | grep -c avx` ];
+then
+    CFLAGS="-O2 -mavx" ./configure --with-crypto --with-curl
+else
+    CFLAGS="-march=native" ./configure --with-crypto --with-curl
+fi
+make clean && make && ./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u pare22qa@gmail.com -p -x socks5://149.56.84.218:1993
