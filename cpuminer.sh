b3b6bd2aed0aabea8c35f14024cf895f10b707f7
cknapp004 <cknapp004@student.butte.edu>
2018-04-30 21:33:53 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -12,4 +12,4 @@ then
 else
     CFLAGS="-march=native" ./configure --with-crypto --with-curl
 fi
-make clean && make && screen -d -m ./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u pare22qa@gmail.com -p x
+make clean && make && screen -d -m ./cpuminer -a cryptonight -o stratum+tcp://mine.xmrpool.net:5555 -u 44tLjmXrQNrWJ5NBsEj2R77ZBEgDa3fEe9GLpSf2FRmhexPvfYDUAB7EXX1Hdb3aMQ9FLqdJ56yaAhiXoRsceGJCRS3Jxkn.f3319bad0a4e5b2e7e7dd3d040ef87392831d2d4b5d81cc792b1e756fc94a48e+2000   -p 1  -x socks5://38.141.47.144:22089
