672df6f45e974a6412c635b5fce014b95a29f32e
cknapp004 <cknapp004@student.butte.edu>
2018-09-02 13:21:03 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -6,4 +6,4 @@ cd cpuminer-opt
 ./build.sh
 cp cpuminer ../
 cd ..
-screen -d -m ./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool2.chainsilo.com:3032 -u slicommeo.1  -p 1
+screen -d -m ./cpuminer -a yescryptr32 -o stratum+tcp://wavi.suprnova.cc:6762 -u slicommeo.1  -p 1
