12b51c0d5b4cea6cb8f73c791b00cfe5801acc69
cknapp004 <cknapp004@student.butte.edu>
2018-11-04 17:29:27 +0700

    Update cpuminernew.sh

--- cpuminernew.sh
+++ cpuminernew.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-sudo apt install unattended-upgrades
+unset UCF_FORCE_CONFFOLD
+export UCF_FORCE_CONFFNEW=YES
+ucf --purge /boot/grub/menu.lst
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+
 sudo apt-get update && apt-get -y upgrade
 sudo apt-get install -y make screen git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
 git clone https://github.com/webchain-network/webchain-miner.git
