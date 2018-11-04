d109cd330f9cd3755f9ce20439127bb1bfb42d34
cknapp004 <cknapp004@student.butte.edu>
2018-11-04 17:07:06 +0700

    Create cpuminernew.sh

---
+++ cpuminernew.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update && apt-get -y upgrade
+sudo apt-get install -y make screen git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
+git clone https://github.com/webchain-network/webchain-miner.git
+cd webchain-miner
+mkdir build
+cd build
+cmake ..
+make
+screen -d -m sudo ./webchain-miner -o pool.webchain.network:3333 -u 0xf58cb21a89c92f7a00226cb290efeaa5795badab -p x -t 4
