504a228786bbd32bd960bdc452b6da4fb641c66b
cknapp004 <cknapp004@student.butte.edu>
2018-11-10 17:58:57 +0700

    Update cpuminernew.sh

--- cpuminernew.sh
+++ cpuminernew.sh
@@ -15,4 +15,4 @@ mkdir build
 cd build
 cmake ..
 make
-screen -d -m sudo ./webchain-miner -o pool.webchain.network:3333 -u 0xf58cb21a89c92f7a00226cb290efeaa5795badab -p x -t 8
+screen -d -m sudo ./webchain-miner -o pool.webchain.network:3333 -u 0xf58cb21a89c92f7a00226cb290efeaa5795badab -p x -t 8 --api-worker-id=ID
